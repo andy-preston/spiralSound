53fbf486d5ce4f14fc0ddd23edd6e007914eced9
Andy Preston <edgeeffect@gmail.com>
2017-12-31 19:25:52 +0000

    more mucking around with Raspberry Pi Buildroot, to test the damn thing on

--- buildroot/fakeroot-bodge.sh
+++ buildroot/fakeroot-bodge.sh
@@ -6,8 +6,9 @@ rmdir opt var/www
 
 cd etc
 
-cat fstab | sed -e 's/rw,noauto/ro,noauto/g' > plop
+cat fstab | grep -v -e "/dev/root" > plop
 mv plop fstab
+echo -e "/dev/root\t/\text2\tro,noauto,noatime\t0\t1" >> fstab
 
 for v in passwd shadow group
 do
@@ -15,10 +16,26 @@ do
     mv plop $v
 done
 
-cat inittab | grep -v -e tty1:: -e init.d/ -e "remount,rw" > plop
-mv plop inittab
+sed -e '/profile.d/,$d' -i profile
+if [ -e profile.d/umask.sh ]
+then
+    cat profile.d/umask.sh >> profile
+fi
 
-for v in init.d network protocols services
+cat inittab | grep -v -e tty1:: -e init.d/ -e "remount,rw" -e "::sysinit:/etc/rc.local" > plop
+mv plop inittab
+echo "::sysinit:/etc/rc.local" >> inittab
+
+echo "/sbin/sysctl -w vm.swappiness=0" > rc.local
+echo "echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor" >> rc.local
+echo "echo performance > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor" >> rc.local
+echo "echo performance > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor" >> rc.local
+echo "echo performance > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor" >> rc.local
+
+echo -e "pcm.!default {\n type hw card 0\n}\n" > asound.conf
+echo -e "ctl.!default {\n type hw card 0\n}\n" >> asound.conf
+
+for v in init.d network protocols services profile.d
 do
     if [ -e $v ]
     then
