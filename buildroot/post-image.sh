c143e616f9148ec3a22214a57540c29f368be32b
Andy Preston <edgeeffect@gmail.com>
2017-12-31 20:46:17 +0000

    tidied up the DAC+ buildroot config

--- buildroot/post-image.sh
+++ buildroot/post-image.sh
@@ -5,7 +5,8 @@ sed -e "/^dtparam=audio=on/# dtparam=audio=on/g" -i "${BINARIES_DIR}/rpi-firmwar
 
 # enable iQaudio DAC+
 
+grep -i -v -e iqaudio -e i2s-mmap "${BINARIES_DIR}/rpi-firmware/config.txt" > plop
+mv plop "${BINARIES_DIR}/rpi-firmware/config.txt"
 echo "# enable iQaudio DAC+" >> "${BINARIES_DIR}/rpi-firmware/config.txt"
 echo "dtoverlay=i2s-mmap" >> "${BINARIES_DIR}/rpi-firmware/config.txt"
 echo "dtoverlay=iqaudio-dacplus" >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-
